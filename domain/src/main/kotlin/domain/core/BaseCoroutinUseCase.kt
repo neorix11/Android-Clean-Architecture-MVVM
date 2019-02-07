7f0f71bc532efeb0d83a3e009284a11332cca882
Sean Larson <atghost11@gmail.com>
2019-02-07 08:49:37 -0500

    Returned Generics to Standards

--- domain/src/main/kotlin/domain/core/BaseCoroutinUseCase.kt
+++ domain/src/main/kotlin/domain/core/BaseCoroutinUseCase.kt
@@ -6,13 +6,11 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.CoroutineContext
 
-abstract class BaseCoroutinUseCase<in Params, out Type>
-    (private val providedCoroutinContext: CoroutineContext? = null) : CoroutineScope,
-    BaseUseCase<Params, Type> {
+abstract class BaseCoroutinUseCase<in P, out T>(private val providedCoroutinContext: CoroutineContext? = null) : CoroutineScope, BaseUseCase<P, T> {
 
     private val job: Job = Job()
     override val coroutineContext: CoroutineContext
         get() = providedCoroutinContext ?: Dispatchers.Main + job
 
-    suspend operator fun invoke(params: Params): Type = withContext(Dispatchers.Default) { execute(params) }
+    suspend operator fun invoke(params: P): T = withContext(Dispatchers.Default) { execute(params) }
 }
--- domain/src/main/kotlin/domain/core/BaseUseCase.kt
+++ domain/src/main/kotlin/domain/core/BaseUseCase.kt
@@ -1,5 +1,5 @@
 package com.bluelampcreative.domain.core
 
-interface BaseUseCase<in Params, out Type> {
-    fun execute(params: Params): Type
+interface BaseUseCase<in P, out T> {
+    fun execute(params: P): T
 }
